5f11ef55aef780a20fe0289f23303193134ca3b4
Paul Shen <paul@mnml0.com>
2013-06-28 14:07:02 -0700

    Add big numbers table test

--- tests.js
+++ tests.js
@@ -19,5 +19,56 @@ var TESTS = {
     timerEnd();
     React.unmountAndReleaseReactRootNode(mountNode);
     onDone();
+  },
+
+  numbersTable: function(timerStart, timerEnd, onDone) {
+    var mountNode = document.getElementById('mountNode');
+
+    var data = [];
+    for (var ii = 0; ii < 50; ii++) {
+      data[ii] = [];
+      for (var jj = 0; jj < 50; jj++) {
+        data[ii][jj] = ii * 50 + jj;
+      }
+    }
+    var Cell = React.createClass({
+      render: function() {
+        return React.DOM.td({}, this.props.value);
+      }
+    });
+    var Row = React.createClass({
+      render: function() {
+        var cells = [];
+        for (var jj = 0; jj < this.props.data.length; jj++) {
+          cells.push(Cell({value: this.props.data[jj]}));
+        }
+        return React.DOM.tr({}, cells);
+      }
+    });
+    var Table = React.createClass({
+      render: function() {
+        var rows = [];
+        for (var jj = 0; jj < this.props.data.length; jj++) {
+          rows.push(Row({data: this.props.data[jj]}));
+        }
+        return React.DOM.table({}, rows);
+      }
+    });
+
+    var numRender = 0;
+    var renderPass = function() {
+      timerStart();
+      React.renderComponent(Table({data: data}), mountNode);
+      timerEnd();
+
+      numRender++;
+      if (numRender === 4) {
+        React.unmountAndReleaseReactRootNode(mountNode);
+        onDone();
+      } else {
+        setTimeout(renderPass, 50);
+      }
+    }
+    renderPass();
   }
 };
